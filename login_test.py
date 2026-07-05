af3b5b473b808f7f6d6886642c8855a447fcd236
Joel Pimentel <j.pimentel@a1qa.com>
2026-07-04 22:24:15 -0600

    Add login assertions and explicit waits

--- login_test.py
+++ login_test.py
@@ -2,6 +2,8 @@ from selenium import webdriver
 from selenium.webdriver.chrome.service import Service
 from webdriver_manager.chrome import ChromeDriverManager
 from selenium.webdriver.common.by import By
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
 
 service = Service(ChromeDriverManager().install())
 driver = webdriver.Chrome(service=service)
@@ -15,6 +17,17 @@ login_button = driver.find_element(By.CLASS_NAME, "radius")
 username.send_keys("tomsmith")
 password.send_keys("SuperSecretPassword!")
 login_button.click()
+
+wait = WebDriverWait(driver, 10)
+
+message = wait.until(
+    EC.visibility_of_element_located((By.ID, "flash"))
+)
+
+#message = driver.find_element(By.ID, "flash")
+
+assert "You logged into a secure area!" in message.text
+#assert "Joel" in message.text
 
 input("Press ENTER to close...")
 driver.quit()
