b51e0b367bf7d8428593e2ed1bf5a3087ce8858b
Joel Pimentel <joel.pimentel5566@gmail.com>
2026-07-12 20:46:33 -0600

    Implement LoginPage Page Object

--- pages/login_page.py
+++ pages/login_page.py
@@ -9,3 +9,17 @@ class LoginPage:
 
     def __init__(self, driver):
         self.driver = driver
+
+    def enter_username(self, username):
+        self.driver.find_element(*self.USERNAME).send_keys(username)
+
+    def enter_password(self, password):
+        self.driver.find_element(*self.PASSWORD).send_keys(password)
+
+    def click_login(self):
+        self.driver.find_element(*self.LOGIN_BUTTON).click()
+
+    def login(self, username, password):
+        self.enter_username(username)
+        self.enter_password(password)
+        self.click_login()
--- tests/test_login.py
+++ tests/test_login.py
@@ -1,3 +1,14 @@
+from selenium import webdriver
+from selenium.webdriver.chrome.service import Service
+from webdriver_manager.chrome import ChromeDriverManager
+
 from pages.login_page import LoginPage
 
-print(LoginPage.USERNAME)
+service = Service(ChromeDriverManager().install())
+driver = webdriver.Chrome(service=service)
+
+driver.get("https://the-internet.herokuapp.com/login")
+
+login_page = LoginPage(driver)
+
+login_page.login("tomsmith", "SuperSecretPassword!")
