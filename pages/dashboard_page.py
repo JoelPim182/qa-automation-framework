67e193ccb44a1d421bf34149d1bdee39eab0c85f
Joel Pimentel <joel.pimentel5566@gmail.com>
2026-08-29 22:48:51 -0600

    refactor: use dashboard heading for page readiness

--- pages/dashboard_page.py
+++ pages/dashboard_page.py
@@ -1,6 +1,4 @@
 from selenium.webdriver.common.by import By
-from selenium.webdriver.support import expected_conditions as EC
-
 from pages.base_page import BasePage
 
 
@@ -8,11 +6,7 @@ class DashboardPage(BasePage):
 
     FLASH_MESSAGE = (By.ID, "flash")
     SUCCESSFUL_LOGIN_MESSAGE = "You logged into a secure area!"
+    DASHBOARD_HEADING = (By.XPATH, "//h2[normalize-space()='Secure Area']")
 
     def wait_until_loaded(self):
-        self.wait(
-            EC.text_to_be_present_in_element(
-                self.FLASH_MESSAGE,
-                self.SUCCESSFUL_LOGIN_MESSAGE
-            )
-        )
+        self.wait_for_visible(self.DASHBOARD_HEADING)
