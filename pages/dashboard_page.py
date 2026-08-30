010a8c61852476ee221457f04fc1e0bbc506b83e
Joel Pimentel <joel.pimentel5566@gmail.com>
2026-08-29 20:56:59 -0600

    refactor: improve dashboard load synchronization

--- pages/dashboard_page.py
+++ pages/dashboard_page.py
@@ -1,4 +1,5 @@
 from selenium.webdriver.common.by import By
+from selenium.webdriver.support import expected_conditions as EC
 
 from pages.base_page import BasePage
 
@@ -6,6 +7,12 @@ from pages.base_page import BasePage
 class DashboardPage(BasePage):
 
     FLASH_MESSAGE = (By.ID, "flash")
+    SUCCESSFUL_LOGIN_MESSAGE = "You logged into a secure area!"
 
     def wait_until_loaded(self):
-        self.wait_for_visible(self.FLASH_MESSAGE)
+        self.wait(
+            EC.text_to_be_present_in_element(
+                self.FLASH_MESSAGE,
+                self.SUCCESSFUL_LOGIN_MESSAGE
+            )
+        )
